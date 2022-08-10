fbbea3b69a16d65ca677819cc5e6be922cb81ea6
opas.keat <opas.keat@gmail.com>
2022-08-10 15:35:25 +0700

    init

---
+++ src/main/java/com/praxis/dapconnect/DapconnectApplication.java
@@ -0,0 +1,13 @@
+package com.praxis.dapconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DapconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DapconnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/praxis/dapconnect/DapconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praxis.dapconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DapconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
