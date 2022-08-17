6735f82987af7c60d3548388cd234ae7b4ae5410
opas.keat <opas.keat@gmail.com>
2022-08-17 20:51:07 +0700

    fix coverage test

--- src/main/java/com/praxis/dapconnect/department/Department.java
+++ src/main/java/com/praxis/dapconnect/department/Department.java
@@ -1,15 +1,14 @@
 package com.praxis.dapconnect.department;
 
 import com.praxis.dapconnect.share.entity.EntityBase;
-import lombok.Builder;
 import lombok.Data;
-import org.hibernate.annotations.*;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 import javax.persistence.Entity;
 import javax.persistence.SequenceGenerator;
 
 @Data
-@Builder
 @Entity
 @SequenceGenerator(name = "default_gen", sequenceName = "department_seq")
 @SQLDelete(sql = "UPDATE department SET deleted = true WHERE id=?")
@@ -27,6 +26,7 @@ public class Department extends EntityBase {
 
     public Department() {
     }
+
     public Department(String name, String createdBy) {
         this.setName(name);
         this.setCreatedBy(createdBy);
