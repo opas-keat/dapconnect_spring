19112a7b32634797e32996223c3939046ab47d0d
opas.keat <opas.keat@gmail.com>
2022-08-17 21:54:32 +0700

    fix coverage test

--- src/test/java/com/praxis/dapconnect/DapconnectApplicationTest.java
+++ src/test/java/com/praxis/dapconnect/DapconnectApplicationTest.java
@@ -1,10 +1,12 @@
 package com.praxis.dapconnect;
 
+import lombok.Generated;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DapconnectApplicationTest {
+    @Generated
     @Test
     void contextLoads() {
     }
